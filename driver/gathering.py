5c2541b77b07e067e3c81481dd5e741227a04a0f
loftmain <54398458+loftmain@users.noreply.github.com>
2020-01-02 15:15:03 +0900

    Revert "Revert "Merge pull request #2 from loftmain/shyoo""
    This reverts commit 8e7f368bd422e6915a026b0c7183c37ae01e211d.

---
+++ driver/gathering.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  2 11:29:32 2020
+
+@author: giho9
+"""
+
+import pandas as pd
+import os
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from pandas import ExcelWriter
+from fredapi import Fred
+
+fred = Fred(api_key='3b2795f81c94f1a105d1e4fc3661a45e')
+
+
+def inspect_index_folder():
+    if not os.path.exists(os.path.join(os.getcwd(), 'independent')):
+        os.mkdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'independent'))
+    folderpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'independent')
+    return folderpath
+
+def inspect_column_file():
+    if not os.path.join(os.path.dirname(os.path.realpath(__file__)), 'column.txt'):    
+        print('column option file does not exist')     
+        return False
+    f = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'column.txt'), 'r')
+    line = f.readline()
+    index_list = list(map(str, (line.split(','))))
+    return index_list
+
+def Gathering():
+    folderpath = inspect_index_folder()
+    index_list = inspect_column_file()
+    if not index_list():
+        return False
+    
+    time_point = datetime.strptime('2000-01-01', '%Y-%m-%d').date()
+    
+    #for index in index_list:
+    for index in index_list:
+        # Get data from fred
+        df = fred.get_series_all_releases(index)
+        #df_info = fred.search(index)
+        
+        # Drop column(realtime_start column)
+        df.drop_duplicates( ["date"], keep="last", inplace=True)
+        
+        # Change column name
+        df['DATE']=df['date'].dt.date
+        df.rename(columns={'value':index}, inplace=True)
+        
+        # Column sort
+        df = df[['DATE',index]]
+        
+        # Drop column(realtime_start column)
+        df.drop(df[df.DATE < time_point].index, inplace=True)
+        df.index = pd.RangeIndex(len(df.index))
+        
+        # Add empty row, Because we use shifted data and use it Machine-Learning
+        df.loc[len(df), 'DATE'] = df['DATE'][len(df) - 1] + relativedelta(months=1)
+        df.loc[len(df), 'DATE'] = df['DATE'][len(df) - 2] + relativedelta(months=2)
+        df.loc[len(df), 'DATE'] = df['DATE'][len(df) - 3] + relativedelta(months=3)
+        
+        # Check for bad data
+        try:
+            df[index+'rate'] = (df[index] / df[index].shift(+1)) -1
+            print(index,": success")
+        except ZeroDivisionError:
+            print(index,": error(data contained zero)")
+            continue
+        
+        # Write to excel file
+        df.to_excel(os.path.join(folderpath,index)+'.xlsx', sheet_name='Sheet1', index=False)
+        #df_info.to_excel(writer, sheet_name='Sheet2', index=False)
+
