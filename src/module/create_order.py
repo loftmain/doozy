aa0485f60401dc843b7a737c178c845348bf1e32
shyoo <39010097+jiho919@users.noreply.github.com>
2020-01-05 21:27:59 +0900

    update order

--- src/module/create_order.py
+++ src/module/create_order.py
@@ -6,11 +6,41 @@ Created on Sun Jan  5 16:27:49 2020
 """
 
 import pandas_datareader.data as web
-import datetime
-import matplotlib.pyplot as plt
+import pandas as pd
 # pip install pandas_datareader
 
-start = datetime.datetime(2010, 1, 1)
-end = datetime.datetime(2019, 12, 31)
+df = pd.read_excel('input_order.xlsx')
+df.rename(columns = {'DATE':'Date'}, inplace=True)
+df = pd.DataFrame(df, columns=['Date', 'HM4UP_predict'])
+
+start = df['Date'][0]
+end = df['Date'][len(df)-1]
 
 data = web.DataReader('^DJI', 'yahoo', start, end)
+data = data.reset_index()
+data['year'] = data["Date"].apply(lambda x: x.year)
+data['month'] = data["Date"].apply(lambda x: x.month)
+data['buy'] = 0
+data['sell'] = 0
+status = False
+
+for x, row in df.iterrows():
+    if row['HM4UP_predict'] == 1:
+        year, month, day = row['Date'].year, row['Date'].month, row['Date'].day
+        
+        temp = data[data['year'] == year]
+        temp = temp[temp['month'] == month]
+        comp_v = temp['Adj Close'][temp.index[0]]
+        
+        for y, _ in temp.iterrows():
+           
+            if temp['Adj Close'][y]/comp_v >= 1.04:
+                data['buy'][temp.index[0]] = 1
+                data['sell'][y] = 1
+                status = True
+                break
+            
+        if status == False:
+            data['buy'][temp.index[0]] = 1
+            data['sell'][temp.index[-1]] = 1
+
