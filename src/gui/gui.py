c29c8e01fcd133921a216019db65a3cd2ae7a4cd
shyoo <39010097+jiho919@users.noreply.github.com>
2020-01-09 12:03:43 +0900

    update & merge

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -16,6 +16,10 @@ from PySide2.QtWidgets import (QMainWindow, QAction, QActionGroup, QToolBar,
                                QLabel,QMessageBox, QTextEdit)
 from PySide2.QtGui import QIcon
 from PySide2.QtCore import QSettings
+import PySide2
+dirname = os.path.dirname(PySide2.__file__)
+plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
 
 class MainWindow(QMainWindow):
     def __init__(self,parent=None):
